353f9281d08e27fad595cc6d0311c48c9208020d
imanav10 <manavrj.07@gmail.com>
2024-07-09 17:04:06 +0530

    hhwqh

--- Huffman-encoding/Tree/tree.c
+++ Huffman-encoding/Tree/tree.c
@@ -1,12 +1,8 @@
 #include <stdio.h>
-
-// Assuming the existence of functions to get a node, get the minimum, and insert into some structure.
-// These functions need to be defined elsewhere in your code.
 int getNode(void);
 int getMin(int Q);
 void insert(int Q, int temp);
 
-// Assuming 'f', 'left', and 'right' are arrays that have been defined and initialized elsewhere in your code.
 extern int f[];
 extern int left[];
 extern int right[];
@@ -14,8 +10,8 @@ extern int right[];
 
 int processTree(int c) {
     int n, Q, temp, a, b;
-    n = c; // Assuming |c| was meant to be absolute value or a direct assignment. Here, it's treated as direct assignment.
-    Q = c;
+    n = c;
+	Q = c;
 
     do {
         temp = getNode();
@@ -26,13 +22,9 @@ int processTree(int c) {
         a = left[temp];
         b = right[temp];
 
-        // Assuming the intention was to sum the values of 'f' at indices 'a' and 'b', and store in 'f[temp]'.
         f[tempx] = f[a] + f[b];
 
         insert(Q, temp);
-
-    // Assuming there's a condition for the loop to terminate. 
-    // This is a placeholder; you need to replace 'condition' with an actual condition.
     } while (temp = 0);
 
     return getMin(0);
